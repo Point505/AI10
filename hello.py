7097b0e449dece62e3e1404f981c398505bf3a46
Point505 <kimhaekang20@gmail.com>
2026-01-03 13:51:14 +0900

    1

--- hello.py
+++ hello.py
@@ -1,1 +1,40 @@
 
+
+
+# while문 반복 
+
+"""TreeHit = 0
+
+while TreeHit < 10:
+    TreeHit+= 1
+    
+    print("횟수: %d "TreeHit)
+    
+    if TreeHit == 10:
+        print("End")
+    else: 
+        continue
+    """
+    
+    
+# while문 탈출
+
+coffe = 100
+money = 100
+
+while money:
+    
+    money -= coffe
+   
+    if(money == 0):
+        print("잔고 없음")
+         
+    
+    else:
+       continue
+    
+    
+    
+        
+
+
