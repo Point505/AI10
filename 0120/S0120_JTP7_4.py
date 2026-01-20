9df15fd3a7cef8d2e571cb896d93b56824683cc0
Point505 <kimhaekang20@gmail.com>
2026-01-20 22:37:33 +0900

    22

---
+++ 0120/S0120_JTP7_4.py
@@ -0,0 +1,29 @@
+## 주요 개념 정리
+
+
+
+## 💡 학습하면서 느낀 점
+
+
+
+
+## ❓ 질문 및 궁금한 점
+
+
+
+
+# 간단한 메모장 만들기
+
+
+import sys
+
+
+option = sys.argv[1]
+
+
+if option =='-a':
+    memo = sys.argv[2]
+    
+    f = open('meme.txt','a')
+    
+    
